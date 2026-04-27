0ab1b37da86ed909762aff2d2c1e74efb461c14c
Archalecto <sevcenkoviktor30@gmail.com>
2026-04-28 03:37:19 +0500

    fix csrf trusted origins

--- Qwim_store/settings.py
+++ Qwim_store/settings.py
@@ -83,4 +83,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-CSRF_TRUSTED_ORIGINS = ['https://qwim.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://qwim.up.railway.app']
